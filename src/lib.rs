e79426581b4f13959129b388c0cbcdc2697d9ab8
Victor Mihalache <hey@veeque.dev>
2025-03-27 21:38:22 +0100

    ading stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,23 @@
+use wasm_bindgen::prelude::wasm_bindgen;
 use worker::*;
 
-#[event(fetch)]
-async fn fetch(
-    _req: Request,
-    _env: Env,
-    _ctx: Context,
-) -> Result<Response> {
+use argon2::{
+    password_hash::{rand_core::OsRng, PasswordHasher, SaltString},
+    Argon2,
+};
+
+#[wasm_bindgen]
+pub async fn hash(password: String) -> String {
     console_error_panic_hook::set_once();
-    Response::ok("Hello World!")
+
+    let password = password.as_bytes();
+    let salt = SaltString::generate(&mut OsRng);
+
+    // Argon2 with default params (Argon2id v19)
+    let argon2 = Argon2::default();
+
+    // Hash password to PHC string ($argon2id$v=19$...)
+    let password_hash = argon2.hash_password(password, &salt)?.to_string();
+
+    return password_hash;
 }
