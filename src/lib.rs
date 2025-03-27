90538227a6f63b7983f7e28646b760afbad852e1
Victor Mihalache <hey@veeque.dev>
2025-03-27 22:07:05 +0100

    works a bit more

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,33 @@ use wasm_bindgen::prelude::wasm_bindgen;
 use worker::*;
 
 use argon2::{
-    password_hash::{rand_core::OsRng, PasswordHasher, SaltString},
+    password_hash::{PasswordHasher, SaltString},
     Argon2,
 };
 
 #[wasm_bindgen]
-pub async fn hash(password: String) -> String {
+pub async fn hash(password: String, salt: String) -> String {
     console_error_panic_hook::set_once();
 
     let password = password.as_bytes();
-    let salt = SaltString::generate(&mut OsRng);
 
     // Argon2 with default params (Argon2id v19)
     let argon2 = Argon2::default();
 
     // Hash password to PHC string ($argon2id$v=19$...)
-    let password_hash = argon2.hash_password(password, &salt)?.to_string();
+    let salt = match SaltString::from_b64(&salt) {
+        Ok(salt) => salt,
+        Err(error) => {
+            console_log!("Error decoding salt: {}", error);
+            return "Decoding failed".to_string();
+        }
+    };
 
-    return password_hash;
+    match argon2.hash_password(password, &salt) {
+        Ok(hash) => hash.to_string(),
+        Err(error) => {
+            console_log!("Error hashing password: {}", error);
+            "Hashing failed".to_string()
+        }
+    }
 }
